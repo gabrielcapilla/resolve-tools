25940d07153b70ae9f49200ceb989c7199b4d376
Gabriel Capilla <170623949+gabrielcapilla@users.noreply.github.com>
2024-07-31 08:52:19 +0200

    feat: Install script for Dolphin Services Menu

---
+++ tools/pkg-scripts/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+readonly LOCAL_DIR="$HOME/.local/share/kio/servicemenus/"
+
+function main() {
+  local DESKTOP_FILE
+  DESKTOP_FILE="$(dirname "$0")/resolvetools.desktop"
+
+  local SCRIPT_FOLDER
+  SCRIPT_FOLDER="$(dirname "$0")/resolvetools-servicemenus"
+
+  cp "$DESKTOP_FILE" "$LOCAL_DIR"
+  cp -r "$SCRIPT_FOLDER" "$LOCAL_DIR"
+
+  echo "Resolve Tools 0.1.0 installed"
+}
+
+main
