863178014d5627e37f9be42cfee2b695751f1bb1
Gabriel Capilla <170623949+gabrielcapilla@users.noreply.github.com>
2025-07-17 06:51:14 +0200

    Refactor: Slight modification of stderr

--- src/functions/kdialog_error.sh
+++ src/functions/kdialog_error.sh
@@ -1,10 +1,10 @@
 function stderr() {
   # Log an error message and exit with a non-zero status
-  local -r error_message="$*"
+  local -r error_message="Error: $*"
 
   # Show the error box using kdialog
   kdialog --error "$error_message" \
-    --title "Resolve Tools Error"
-
+    --title "Resolve Tools Error" \
+    --icon "error"
   exit 1
 }
